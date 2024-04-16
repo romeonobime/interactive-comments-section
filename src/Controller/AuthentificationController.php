da4ac23517bedcb9f9305c768ac7be248016684e
Romeo Nobime <romeonobime@pop-os.lan>
2024-04-16 11:20:08 +0200

    changed AuthentificationController

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AuthentificationController extends AbstractController
 {
-    #[Route('/authentification/signup', name: 'app_authentification')]
-    public function index(): Response
+    #[Route('/authentification/signup', name: 'app_authentification_signup')]
+    public function signup(): Response
     {
         return $this->render('pages/authentification/signup.html.twig');
     }
