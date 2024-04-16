c0b18588a2c658486901acca525a2dea78c326b1
Romeo Nobime <romeonobime@pop-os.lan>
2024-04-16 11:51:05 +0200

    added TheSignup

---
+++ src/Form/SignupType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class SignupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
---
+++ src/Twig/Components/TheSignupForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\User;
+use App\Form\SignupType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[AsLiveComponent]
+class TheSignupForm extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    #[LiveProp]
+    public ?User $initialFormData = null;
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(SignupType::class, $this->initialFormData);
+    }
+
+    #[LiveAction]
+    public function signup(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, Security $security)
+    {
+        $this->submitForm();
+        /** @var User $user */
+        $user = $this->getForm()->getData();
+        $unhashedPassword = $user->getPassword();
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $unhashedPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $security->login($user);
+
+        return $this->redirectToRoute('app_browsing_home');
+    }
+}
