74aa7236290a1152ce97a501f8bd4d7afd4b0a6c
Romeo Nobime <romeonobime@pop-os.lan>
2024-04-16 19:47:52 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
+    colors: {
+      'primary': '#5357B6',
+      'primary-light': '#C5C6EF',
+      'danger': '#ED6368',
+      'danger-light': '#FFB8BB',
+      'neutral-1': '#E9EBF0',
+      'neutral-1-very-light': '#F5F6FA',
+      'neutral-2': '#67727E',
+      'neutral-2-dark': '#334253',
+      'white': '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
